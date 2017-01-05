503c7dbde4eb037504b6d43779e25e50b0f0ce1f
Diogo Carvalhais <mini_digo@hotmail.com>
2017-01-05 10:14:11 +0000

    Teste

---
+++ POO_AgeOfColognes/POO_AgeOfColognes/Celula.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+class Celula{
+	vector<>
+public:
+	Celula();
+	~Celula();
+};
+
---
+++ POO_AgeOfColognes/POO_AgeOfColognes/Objeto.h
@@ -0,0 +1,9 @@
+#pragma once
+class Objeto{
+	int x, y; //coordenadas
+	int cor; //cor
+public:
+	Objeto(int x, int y, int cor);
+	~Objeto();
+};
+
